bcae5fec3e5b032cfd6502694e2d8981d3f152b5
wanghui <wanghui@kkworld.com>
2018-10-09 19:57:17 +0800

    datasource

--- anyway-admin/src/main/java/com/anyway/admin/AnywayAdminApplication.java
+++ anyway-admin/src/main/java/com/anyway/admin/AnywayAdminApplication.java
@@ -2,11 +2,12 @@ package com.anyway.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class AnywayAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnywayAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AnywayAdminApplication.class, args);
+    }
 }
