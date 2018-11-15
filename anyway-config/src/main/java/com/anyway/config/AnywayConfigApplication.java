fc40ac954fa05a60f035a917ad3ef7d203c136c1
wanghui <wh112523@163.com>
2018-11-15 15:52:17 +0800

    hui

---
+++ anyway-config/src/main/java/com/anyway/config/AnywayConfigApplication.java
@@ -0,0 +1,12 @@
+package com.anyway.config;
+
+/**
+ * @author: wang_hui
+ * @date: 2018/10/25 下午3:02
+ */
+public class AnywayConfigApplication {
+
+    public static void main(String[] args) {
+    }
+
+}
